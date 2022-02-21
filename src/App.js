ba68a038ed89ed2f8bb21811f73838de2c048d59
jess <jessica.elessa@gmail.com>
2022-02-21 18:51:40 +0100

    setting pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar></NavBar>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/login" component={Login}></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
-
 export default App;
