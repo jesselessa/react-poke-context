f09607e415ed84e30bc6b5364cb4702a6f33f794
jess <jessica.elessa@gmail.com>
2022-02-22 04:21:14 +0100

    form completed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,3 +1,40 @@
+import { useForm } from "react-hook-form";
+
 export default function Login() {
-  return <div></div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register("username", { required: true })}
+          type="text"
+          id="username"
+          name="username"
+          maxLength={15}
+          placeholder="Username"
+        />
+        {errors.username && (
+          <span>Username must not be more than fifteen characters</span>
+        )}
+        <input
+          {...register("password", { required: true })}
+          type="password"
+          id="password"
+          name="password"
+          minLength={6}
+          placeholder="Password"
+        />
+        {errors.password && (
+          <span>Password needs to be more than six characters</span>
+        )}
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
