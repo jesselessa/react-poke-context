742eb473448065b583a665f107e9c11fe86f4ed2
jess <jessica.elessa@gmail.com>
2022-02-22 16:18:36 +0100

    finished context initialization

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { createContext } from "react";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Login from "./components/Login";
 
+// Context : user connection status
+export const UserContext = createContext({ isLogged: false });
+
 function App() {
+  // State de App.js :
+  const [isLogged, setLogged] = useState(false);
+
+  // Function wich changes the context according to the user's connection status :
+  const setAuth = () => {
+    if (isLogged === false) {
+      setLogged(true);
+    } else {
+      setLogged(false);
+    }
+  };
+
+  // Context and function saved in a variable :
+  const value = {
+    isLogged: isLogged,
+    setAuth: setAuth,
+  };
+
   return (
-    <BrowserRouter>
-      <NavBar></NavBar>
-      <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route exact path="/login" component={Login}></Route>
-      </Switch>
-    </BrowserRouter>
+    <UserContext.Provider value={value}>
+      {/* By setting a value attribute equal to the variable named value in UserContext,the latter will be able to be used by other components */}
+      <BrowserRouter>
+        <NavBar></NavBar>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/login" component={Login}></Route>
+        </Switch>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 export default App;
