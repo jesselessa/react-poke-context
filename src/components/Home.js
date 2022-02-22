d3e709c279a0be4f89d35bd677817687d297585a
jess <jessica.elessa@gmail.com>
2022-02-22 04:41:59 +0100

    useState & useEffect

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,19 @@
+import { useState, useEffect } from "react";
+
 export default function Home() {
+  // Hooks
+  const [pokemon, setPokemon] = useState({});
+  const [number, setNumber] = useState(1);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${number}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setPokemon(res);
+        console.log(pokemon);
+      })
+      .catch((err) => console.log(err));
+  }, [number]);
+
   return <div></div>;
 }
