0f5892f813765da5cd556fb6b6bc6bf2229cda2c
jess <jessica.elessa@gmail.com>
2022-02-22 04:55:36 +0100

    function getRandomNumber & render completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,5 +15,21 @@ export default function Home() {
       .catch((err) => console.log(err));
   }, [number]);
 
-  return <div></div>;
+  const getRandomNumber = () => {
+    return setNumber(Math.floor(Math.random() * 100) + 1);
+  };
+
+  return (
+    <>
+      <button type="submit" onClick={getRandomNumber}>
+        GET A POKEMON
+      </button>
+      <div>
+        <p>Name : {pokemon.name}</p>
+        <p>Height :{pokemon.height}</p>
+        <p>Weight : {pokemon.weight}</p>
+        <p>Type : {pokemon.types[0].type.name}</p>
+      </div>
+    </>
+  );
 }
